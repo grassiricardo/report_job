7353be688908c92c937aef3bf0a05a8f22936eb5
Ricardo Grassi <grassiricardorg@gmail.com>
2017-10-30 22:24:38 -0200

    finish learning railsJobs

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,20 +4,9 @@ class ReportsController < ApplicationController
   end
 
   def create
-    #Adicional para que demore um pouco o Job
-    sleep 8
+    ReporteJob.perform_later
 
-    unless File.directory?("#{Rails.root.join('public')}/reports")
-      Dir.mkdir "#{Rails.root.join('public')}/reports"
-    end
-    code = (0...8).map { (65 + rand(26)).chr }.join
-    out_file = File.new("#{Rails.root.join('public')}/reports/report#{code}.html", "w")
-    out_file.puts("<p>Your Report Here - code: <b>#{code}</b></p>")
-    out_file.puts("<hr>")
-    out_file.puts("e-Book: Guia de Gems OneBitCode - <a href='http://onebitcode.com/guia-onebitcode-de-gems/'>Baixar Agora :)</a>")
-    out_file.close
-
-    # flash[:notice] = "Estamos processando seu relatório \\o/"
+    flash[:notice] = "Estamos processando seu relatório \\o/"
     redirect_to '/reports'
   end
 end
